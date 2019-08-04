c4c5e8a2bb05f896fafcd8140ff86b40e7d1b55b
hyperclick <forspam28@ya.ru>
2019-08-04 16:22:04 +0300

    server: add command line args

--- client_server/HttpServer/Program.cs
+++ client_server/HttpServer/Program.cs
@@ -14,7 +14,18 @@ namespace HttpServer
     {
         public static void Main(string[] args)
         {
+            if (args.Length != 1 || args[0] != "-s")
+            {
+                ShowUsage();
+                return;
+            }
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("httpserver -s");
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
