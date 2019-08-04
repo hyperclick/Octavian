ce7a664b0743af1660a685008271d90674b94b73
hyperclick <forspam28@ya.ru>
2019-08-04 15:18:12 +0300

    client: add command line args

--- client_server/client_server/Program.cs
+++ client_server/client_server/Program.cs
@@ -1,14 +1,26 @@
 ﻿using Server;
 using System;
 
-namespace client_server
+namespace client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 3 || args[1] != "-c")
+            {
+                ShowUsage();
+                return;
+            }
+
             var server = Factory.Create();
             server.Send("Hello World!");
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usasge:");
+            Console.WriteLine($"client -c <path to file to send>");
         }
     }
 }
